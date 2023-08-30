8a4554a1a896863f030673e95edba3771509dcf6
= <=>
2023-08-31 07:04:06 +0800

    chore: Updated setup.sh script to fix syntax errors

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 
 # Setting the cronjob
 cron_filepath = "/var/spool/cron/crontab/"
-if !grep -q "store.json" "$cron_filepath" then;(crontab -l;echo "0 * * * * rm -rf /root/wabot/database/store.json /root/wabot/database/db.json") | crontab;fi
+if ! grep -q "store.json" "$cron_filepath"; then (crontab -l; echo "0 * * * * rm -rf /root/wabot/database/store.json /root/wabot/database/db.json") | crontab; fi
